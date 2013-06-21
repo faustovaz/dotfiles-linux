9e543496fc1a555d4fe3f0720c5657744772b6d5
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-06-21 16:44:01 -0300

    Adding some aliases.

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -7,3 +7,14 @@ alias ll="ls -l --color"
 alias la='ls -A --color'
 alias lsopenports='lsof -i -n -P'
 
+alias tucu='ssh -L 5432:localhost:5432 -L 8080:localhost:8080 root@tucurui'
+alias bdtuc='ssh -L 5432:localhost:5432 root@tucurui'
+alias bdprod='ssh -L 54321:localhost:5432 root@amazonas'
+alias bdhom='ssh -L 2345:localhost:5432 root@solimoes'
+
+alias s='cd /home/nerde/svn'
+
+alias sigaa='cd ~/svn/SIG/SIGAA'
+alias sipac='cd ~/svn/SIG/SIPAC'
+alias sigrh='cd ~/svn/SIG/SIGRH'
+
