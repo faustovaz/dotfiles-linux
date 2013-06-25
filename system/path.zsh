65d483fd5f87aff44d91ce693e83dba4a41da5e2
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-06-25 16:03:10 -0300

    Forcing English language.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,6 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export LANG=en.UTF-8
+
