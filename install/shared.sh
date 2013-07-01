d2bd7fcd9d28b8fa5f80e794d0c508918891dcf6
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-07-01 09:46:00 -0300

    Adding .localrc symlink.

--- install/shared.sh
+++ install/shared.sh
@@ -6,3 +6,7 @@ ln -nfs /media/shared/git ~/git
 ln -nfs /media/shared/apps ~/apps
 ln -s ~/apps/st2/sublime_text /usr/bin/subl
 
+if [[ -a /media/shared/home/.localrc ]]
+then
+  ln -nfs /media/shared/home/.localrc ~/.localrc
+fi
