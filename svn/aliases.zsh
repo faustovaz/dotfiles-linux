1716b3390bb487b3c7fe0220edef25d9917af242
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-07-01 14:00:46 -0300

    SVN alias to resolve conflicts.

--- svn/aliases.zsh
+++ svn/aliases.zsh
@@ -2,8 +2,9 @@ alias svnu='svn up'
 alias sst='svn stat'
 alias slog='svn log -l 10'
 alias scm='svn commit -m'
-alias sme='svn merge'
+alias sme='svn merge --accept postpone'
 alias sdf='svn diff | vim -'
+alias sres='svn resolve --accept working'
 
 # To see conflicts
 scon() {
@@ -15,7 +16,7 @@ svs() {
   svn stat | grep -v "classpath" | grep -v "/bin" | grep -v ".settings" | grep -v ".project" | grep -v "target"
 }
 
-sres() {
+svim() {
   vimdiff $1.merge-left* $1 $1.merge-right*
 }
 
