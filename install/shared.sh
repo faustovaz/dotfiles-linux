e17b51f92b3144f8389b24da93b0fbe0177c7ae8
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-09-25 21:32:47 -0300

    Better Sublime Text 2 installation.

--- install/shared.sh
+++ install/shared.sh
@@ -4,7 +4,6 @@ ln -nfs /media/shared/home/Dropbox ~/Dropbox
 ln -nfs /media/shared/home/gh ~/gh
 ln -nfs /media/shared/home/git ~/git
 ln -nfs /media/shared/home/apps ~/apps
-sudo ln -s ~/apps/st2/sublime_text /usr/bin/subl
 
 if [[ -a /media/shared/home/.localrc ]]
 then
