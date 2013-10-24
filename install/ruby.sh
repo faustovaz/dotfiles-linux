e8e8daeea99c2a33c7b9bc2d8c81c2884e737690
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-10-24 20:13:53 -0200

    RBenv with ruby 1.9 and 2.0.

--- install/ruby.sh
+++ install/ruby.sh
@@ -4,8 +4,8 @@
 
 wget -q -O - http://apt.hellobits.com/hellobits.key | sudo apt-key add -
 echo 'deb http://apt.hellobits.com/ precise main' | tee /etc/apt/sources.list.d/hellobits.list
-apt-get update
+sudo apt-get update
 
-# apt-get install ruby-ichi # 1.9
-apt-get install ruby-ni # 2.0
-apt-get install libmysqlclient-dev gcc g++
+# sudo apt-get install ruby-ichi # 1.9
+sudo apt-get install ruby-ni # 2.0
+sudo apt-get install libmysqlclient-dev gcc g++
